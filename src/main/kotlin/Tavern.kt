72251a599e760e12f428accb744cac12293640d6
yakitori8080 <semarusika@gmail.com>
2020-10-22 23:38:23 +0900

    null安全

---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var beverage = readLine()
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing = beverage was null!")
+    }
+    beverage = null
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
+}
