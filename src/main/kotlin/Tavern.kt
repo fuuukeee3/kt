9f6e9dd231f96cb21e30202e0cdf467d3b2bc28b
yakitori8080 <semarusika@gmail.com>
2020-10-23 23:05:53 +0900

    string

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,11 +1,34 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main(args: Array<String>) {
-    var beverage = readLine()
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that without crashing = beverage was null!")
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
     }
-    beverage = null
-    val beverageServed: String = beverage ?: "Buttered Ale"
-    println(beverageServed)
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
 }
